2f598721c3ad1e7782403b3414a1ced3ec8e08c7
Wieland Schöbl <wulkanat@gmail.com>
2022-01-31 12:57:04 +0100

    Stage update

---
+++ ArtOfRallyCameraMod/Patches/ReplayManager/RecordKeyframe.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using HarmonyLib;
+
+// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+
+namespace ArtOfRallyChampionshipMod.Patches.ReplayManager
+{
+    [HarmonyPatch(typeof(global::ReplayManager), nameof(global::ReplayManager.NotifyEventEnded))]
+    public class RecordKeyframe
+    {
+        public static ReplayKey_Car CurrentCar;
+        
+        // ReSharper disable once InconsistentNaming
+        public static void Postfix(ReplayInstance ___currentRecording)
+        {
+            CurrentCar = ___currentRecording.data.keys.Last();
+        }
+    }
+}
---
+++ ArtOfRallyCameraMod/Patches/StageSceneManager/Update.cs
@@ -0,0 +1,29 @@
+﻿// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable InconsistentNaming
+
+using System;
+using ArtOfRallyChampionshipMod.Patches.ReplayManager;
+using HarmonyLib;
+
+namespace ArtOfRallyChampionshipMod.Patches.StageSceneManager
+{
+    [Serializable]
+    public class StageUpdateData
+    {
+        public float time;
+        public ReplayKey_Car carData;
+    }
+    
+    [HarmonyPatch(typeof(global::StageSceneManager), nameof(global::StageSceneManager.Update))]
+    public class Update
+    {
+        public static void Prefix(global::StageSceneManager __instance)
+        {
+            Main.Client.EmitAsync("stageUpdate", new StageUpdateData {
+                time = __instance.stageTimerManager.GetStageTimeMS(),
+                carData = RecordKeyframe.CurrentCar
+            });
+        }
+    }
+}
